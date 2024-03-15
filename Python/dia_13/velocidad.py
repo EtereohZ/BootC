b25bdd3fe40def7981670e2e1fa9449ab6cbb562
joseto <josetomas.w9@gmail.com>
2024-03-15 20:52:18 -0300

    Le dedico este commit al profe Jaime que me saco de la tortura

--- Python/dia_13/velocidad.py
+++ Python/dia_13/velocidad.py
@@ -7,15 +7,34 @@ velocidad = [25, 12, 19, 16, 11, 11, 24, 1,
 11, 10, 18, 10, 14, 5, 23, 20, 23, 21]
 
 #Calcular el promedio
-promedio = sum(velocidad) / len(velocidad)
+# promedio = sum(velocidad) / len(velocidad)
 
-#Filtrar las correas sobre el promedio
-indices = []
-def correas():
-    for index, value in enumerate(velocidad):
-        if value > promedio:
+# #Filtrar las correas sobre el promedio
+# indices = []
+# def correas():
+#     for index, value in enumerate(velocidad):
+#         if value > promedio:
+#             (indices.append(index))
+# correas()
+
+# print(f"El promedio es {promedio}")
+# print(f"Las posiciones de las correas que se encuentran sobre el promedio son: {indices}")
+
+def promedio(prom):
+    prmd = sum(prom) / len(velocidad)
+    return prmd
+
+
+def correas(corr, p):
+    indices = []
+    for index, value in enumerate(corr):
+        if value > p:
             (indices.append(index))
-correas()
+    return indices
 
-print(f"El promedio es {promedio}")
-print(f"Las posiciones de las correas que se encuentran sobre el promedio son: {indices}")
+def reporte(velocidad):
+    print(f"El promedio de las correas es {promedio(velocidad)}")
+    print(f"Las posiciones de las correas que se encuentran sobre el promedio son: {correas(velocidad, promedio(velocidad))}")
+    # print(v)
+
+reporte(velocidad)
