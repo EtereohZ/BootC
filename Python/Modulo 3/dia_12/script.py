a74492739e8166153024a090bd97797acd903995
joseto <josetomas.w9@gmail.com>
2024-04-08 12:09:18 -0400

    comentarios

--- Python/Modulo 3/dia_12/script.py
+++ Python/Modulo 3/dia_12/script.py
@@ -10,13 +10,13 @@ try:
     datos = json.load(archivo)
     for d in archivo:
         print(d)
-        instancias.append(Usuario(d.get("nombre"), d.get("apellido"), d.get("email"), d.get("genero"),))
+        instancias.append(Usuario(d.get("nombre"), d.get("apellido"), d.get("email"), d.get("genero"),)) #Intentamos crear las instancias de usuario según los datos entregados
 except Exception as e:
     with open("error.log", "a+") as log:
         now = datetime.now()
-        log.write(f"[{now.strftime('$Y/%m/%d %H:%M:%S')}] Error: {e}\n")
+        log.write(f"[{now.strftime('$Y/%m/%d %H:%M:%S')}] Error: {e}\n")  #Creamos el log de errores con fecha
 else:
-    print("Datos guardados perfectamente")
+    print("Datos guardados perfectamente") #No va a pasar
 
 
 
